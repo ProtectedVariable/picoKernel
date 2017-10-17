3aa9afd050defd4d618357a635880773961f9640
ProtectedVariable <thomas.ibanez33@gmail.com>
2017-10-17 17:35:35 +0200

    Black sceen kernel, very usefull

--- common/util.h
+++ common/util.h
@@ -1,5 +1,10 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
 #include "types.h"
 
 void memset(void* dst, int value, uint count);
 void memcpy(void* dst, void* src, uint count);
 int strncmp(const char *p, const char *q, uint n);
+
+#endif /* end of include guard: _UTIL_H_ */
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,9 @@
 #include "kernel.h"
+#include <util.h>
+#include "gdt.h"
 
 void kernelEntry(multiboot_info_t* inf) {
-  
+  gdt_init();
+  void* frmbf = (void*)0xB8000;
+  memset(frmbf, 0, 0xFA0);
 }
